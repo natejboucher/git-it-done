f811e69e2ee0933693e19b2a6563a035ab8ee2f5
Nate Boucher <natejboucher@gmail.com>
2022-01-02 15:59:56 -0500

    Added error handling for single repo page

--- assets/js/single.js
+++ assets/js/single.js
@@ -3,12 +3,21 @@ var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
 var repoNameEl = document.querySelector("#repo-name");
 
-var getRepoName = function()  {
+var getRepoName = function() {
+    // grab repo name from url query string
     var queryString = document.location.search;
     var repoName = queryString.split("=")[1];
-    getRepoIssues(repoName);
-    repoNameEl.textContent = repoName;
-}
+  
+    if (repoName) {
+      // display repo name on the page
+      repoNameEl.textContent = repoName;
+  
+      getRepoIssues(repoName);
+    } else {
+      // if no repo was given, redirect to the homepage
+      document.location.replace("./index.html");
+    }
+  };
 
 var getRepoIssues = function(repo) {
   // format the github api url
@@ -28,8 +37,8 @@ var getRepoIssues = function(repo) {
       });
     }
     else {
-      console.log(response);
-      alert("There was a problem with your request!");
+    // if not successful, redirect to homepage
+      document.location.replace("./index.html");
     }
   });
 };
